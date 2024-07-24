d19d07b8d4e46056fb79cc2ed38bc70f9ebaca1d
johnLamberts <faithsacredoo3@gmail.com>
2024-07-24 19:43:19 +0800

    first time ko magvue tapos may rat pa

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,35 @@
+<script>
+import { ref } from "vue";
+
+export default {
+  data() {
+    return {
+      message: "",
+    };
+  },
+
+  mounted() {
+    this.$refs.input.focus();
+
+    console.log(this.$refs);
+  },
+};
+</script>
+
+<template>
+  <p>Your message is: {{ message }}</p>
+  <input ref="input" type="text" v-model="message" />
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+
+.horizontal {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: 2rem;
+}
+</style>
